b77272f55756a288360fc9531f2024b82ae945d7
Bill Cotter <billcotter@outlook.com>
2021-12-16 11:41:52 -0500

    first commit

---
+++ pages/events/index.js
@@ -0,0 +1,38 @@
+// import Link from 'next/link';
+import Layout from '@/components/Layout';
+import EventItem from '@/components/EventItem';
+import { API_URL } from '@/config/index';
+
+export default function EventsPage({ events }) {
+  return (
+    <Layout>
+      <h1>Events</h1>
+      {/* <h1>Events</h1> */}
+      {events.length === 0 && <h3>No events to show</h3>}
+
+      {events.map((evt) => (
+        <EventItem key={evt.id} evt={evt} />
+      ))}
+    </Layout>
+  );
+}
+
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+
+  return {
+    props: { events },
+    revalidate: 1,
+  };
+}
+
+// import Layout from '@/components/Layout';
+
+// export default function EventsPage() {
+//   return (
+//     <Layout title='Add New Event'>
+//       <h1>Events</h1>
+//     </Layout>
+//   );
+// }
