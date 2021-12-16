17a7e0225be1f068bc53a961f2ba337dcd8e2b0d
Bill Cotter <billcotter@outlook.com>
2021-12-16 11:49:32 -0500

    added s

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ import { API_URL } from '@/config/index';
 export default function HomePage({ events }) {
   return (
     <Layout>
-      <h1>My Events</h1>
+      <h1>My Eventss</h1>
       {/* <h1>Events</h1> */}
       {events.length === 0 && <h3>No events to show</h3>}
 
